3f5ac09e491c6c3a7641102272fd1b99e7f63f05
David Corrales <github.ferocity308@passmail.com>
2026-06-20 14:12:11 -0500

    ejercicio 14

---
+++ ejercicios_libro/src/boletin01/Ejercicio14.java
@@ -0,0 +1,10 @@
+package boletin01;
+
+import util.Entrada;
+
+public class Ejercicio14 {
+    public static void main(String[] args) {
+        // 14. Pedir una nota de 0 a 10 y mostrarla de la forma: Insuficiente, suficiente, Bien...
+
+    }
+}
